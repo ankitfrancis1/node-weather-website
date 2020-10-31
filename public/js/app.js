e8fe372d3d1b491416eb04c241714816d354822f
ankitfrancis <ankit.francis@accenture.com>
2020-10-31 14:50:36 +0530

    Removed consolelog call

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,3 @@
-console.log("Client Side javascript is running");
-
-// fetch('http://puzzle.mead.io/puzzle').then((response)=>{
-//     response.json().then((data)=>{
-//         console.log(data);
-//     });
-// });
-
-
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 
